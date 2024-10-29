04155276c8f07b9403cb7b62ea5720453d059d97
ranjith reddy <katipallyshivanireddy1@gmail.com>
2024-10-29 16:23:01 +0530

    switch and for statements code created

---
+++ task.js
@@ -0,0 +1,38 @@
+var day=prompt("Enter a number (1-7)for the day of the week");
+switch(day){
+    case '1':
+        console.log("Monday:start of the week");
+        break;
+        case '2':
+            console.log("Tuesday:second day  of the week");
+            break;
+            case '3':
+                console.log("wednesday: third day of the week");
+                break;
+                case '4':
+                    console.log("Thursday: fourty day of the week");
+                    break;
+                    case '5':
+                        console.log("friday: fifth day of the week");
+                        break;
+                        case '6':
+                            console.log("saturdau: fun day of the week");
+                            break;
+                            case '7':
+                                console.log("sunday: holiday");
+                                break;
+                                default:
+                                    console.log("enter correct number")
+
+        
+}
+
+var numbers=[1,2,3,4,5];
+for(var numbers of numbers){
+    console.log(numbers)
+}
+
+var a="hello";
+for(var char of a){
+    console.log(char);
+}
